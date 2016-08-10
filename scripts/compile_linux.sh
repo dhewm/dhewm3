00fe8dc2c2b2a799c3b196d690e6109610b55d49
BielBdeLuna <7318.tk@gmail.com>
2016-08-10 18:10:46 +0200

    added the linux compilation

---
+++ scripts/compile_linux.sh
@@ -0,0 +1,6 @@
+cd ..
+rm -rf build
+mkdir build
+cd build
+
+cmake ../neo
--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -3,3 +3,4 @@ rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../scripts/mingw_toolchain.cmake -DDHEWM3LIBS=../scripts/dhewm3-libs/i686-w64-mingw32 ../neo
+#cmake -DCMAKE_TOOLCHAIN_FILE=../scripts/mingw_toolchain.cmake ../neo
