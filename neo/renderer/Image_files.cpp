3d57e99e0aad2932c4f2599c06acb829376d8e37
Daniel Gibson <metalcaedes@gmail.com>
2017-02-27 20:19:00 +0100

    Hopefully fix built on OSX El Capitan with libjpeg9

--- neo/renderer/Image_files.cpp
+++ neo/renderer/Image_files.cpp
@@ -830,7 +830,7 @@ static void LoadJPG( const char *filename, unsigned char **pic, int *width, int 
 
   /* Step 3: read file parameters with jpeg_read_header() */
 
-  (void) jpeg_read_header(&cinfo, true );
+  (void) jpeg_read_header(&cinfo, (boolean)true);
   /* We can ignore the return value from jpeg_read_header since
    *   (a) suspension is not possible with the stdio data source, and
    *   (b) we passed TRUE to reject a tables-only JPEG file as an error.
