53e0db9f41603a7315fe02a447171c8b2ec2e34d
Daniel Gibson <metalcaedes@gmail.com>
2019-01-04 22:51:12 +0100

    Bump version to 1.5.1pre
    this is not 1.5.0 anymore.

--- neo/framework/Licensee.h
+++ neo/framework/Licensee.h
@@ -41,7 +41,7 @@ If you have questions concerning this license or the applicable additional terms
 #define GAME_NAME						"dhewm 3"		// appears on window titles and errors
 #endif
 
-#define ENGINE_VERSION					"dhewm3 1.5.0"	// printed in console
+#define ENGINE_VERSION					"dhewm3 1.5.1pre"	// printed in console
 
 // paths
 #define	BASE_GAMEDIR					"base"
