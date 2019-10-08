8d27e03c407d6debdb89bb57a75eaac167f85595
Dan Church <amphetamachine@gmail.com>
2019-10-08 15:53:24 -0500

    Fix FTBFS

--- neo/sys/posix/posix_net.cpp
+++ neo/sys/posix/posix_net.cpp
@@ -701,7 +701,7 @@ int	idTCP::Write(void *data, int size) {
 		return -1;
 	}
 
-#if defined(_GNU_SOURCE) && defined(TEMP_FAILURE_RETRY
+#if defined(_GNU_SOURCE) && defined(TEMP_FAILURE_RETRY)
 	// handle EINTR interrupted system call with TEMP_FAILURE_RETRY -  this is probably GNU libc specific
 	if ( ( nbytes = TEMP_FAILURE_RETRY ( write( fd, data, size ) ) ) == -1 ) {
 #else
