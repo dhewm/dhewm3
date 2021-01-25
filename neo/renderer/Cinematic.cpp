7aee2af217a176c46f9ba52f129f5d9f9357b279
Turo Lamminen <turol@iki.fi>
2021-01-25 13:25:37 +0200

    Silence misleading indentation warnings

--- neo/renderer/Cinematic.cpp
+++ neo/renderer/Cinematic.cpp
@@ -939,8 +939,8 @@ unsigned short idCinematicLocal::yuv_to_rgb( int y, int u, int v ) {
 	g = (YY + ROQ_UG_tab[u] + ROQ_VG_tab[v]) >> 8;
 	b = (YY + ROQ_UB_tab[u]) >> 9;
 
-	if (r<0) r = 0; if (g<0) g = 0; if (b<0) b = 0;
-	if (r > 31) r = 31; if (g > 63) g = 63; if (b > 31) b = 31;
+	if (r <0 )  { r = 0;  } if (g < 0)  { g = 0;  } if (b < 0)  { b = 0;  }
+	if (r > 31) { r = 31; } if (g > 63) { g = 63; } if (b > 31) { b = 31; }
 
 	return (unsigned short)((r<<11)+(g<<5)+(b));
 }
@@ -957,8 +957,8 @@ unsigned int idCinematicLocal::yuv_to_rgb24( int y, int u, int v ) {
 	g = (YY + ROQ_UG_tab[u] + ROQ_VG_tab[v]) >> 6;
 	b = (YY + ROQ_UB_tab[u]) >> 6;
 
-	if (r<0) r = 0; if (g<0) g = 0; if (b<0) b = 0;
-	if (r > 255) r = 255; if (g > 255) g = 255; if (b > 255) b = 255;
+	if (r < 0)   { r = 0;   } if (g < 0)   { g = 0;   } if (b < 0)   { b = 0;   }
+	if (r > 255) { r = 255; } if (g > 255) { g = 255; } if (b > 255) { b = 255; }
 
 	return LittleInt((r)+(g<<8)+(b<<16));
 }
