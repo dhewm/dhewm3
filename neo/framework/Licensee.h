fac06f04212b466a450e008df43a0981d080633d
Daniel Gibson <metalcaedes@gmail.com>
2024-03-17 04:15:44 +0100

    1.5.3 RC2

--- neo/framework/Licensee.h
+++ neo/framework/Licensee.h
@@ -41,12 +41,12 @@ If you have questions concerning this license or the applicable additional terms
 #define GAME_NAME						"dhewm 3"		// appears in errors
 #endif
 
-#define ENGINE_VERSION					"dhewm3 1.5.3rc1"	// printed in console, used for window title
+#define ENGINE_VERSION					"dhewm3 1.5.3rc2"	// printed in console, used for window title
 
 #ifdef ID_REPRODUCIBLE_BUILD
 	// for reproducible builds we hardcode values that would otherwise come from __DATE__ and __TIME__
 	// NOTE: remember to update esp. the date for (pre-) releases and RCs and the like
-	#define ID__DATE__  "Feb 04 2024"
+	#define ID__DATE__  "Mar 17 2024"
 	#define ID__TIME__  "13:37:42"
 
 #else // not reproducible build, use __DATE__ and __TIME__ macros
