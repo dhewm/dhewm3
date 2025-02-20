15285e268a0989fcdbd9da00a898cf8f204e36b0
Daniel Gibson <metalcaedes@gmail.com>
2025-02-21 00:44:17 +0100

    Allow using both naturalmatscale and matscalex/y in GUIs
    So far one could only use either naturalmatscale or matscalex/matscaley.
    Just "naturalmatscale 1" lets a texture repeat in its native size, i.e.
    if you have a 64x32 texture and a 320x160 (in the 640x480 GUI coordinate
    system) windowDef, it will be repeated 5 times in each direction.
    If you resize the windowDef to 384x160, it will repeat the texture
    6 times horizontally (and still 5 times vertically).
    
    matscalex/y allow scaling that texture. If both values are set to 2,
    (and naturalmatscale to 1) the 64x32 texture in a 384x160 windowDef
    will be repeated 12 times horizontally and 10 times vertically, at half
    the size per direction.
    Without naturalmatscale it would be repeated 2 times in each direction,
    no matter how big the windowDef is.

--- neo/ui/Window.cpp
+++ neo/ui/Window.cpp
@@ -1160,8 +1160,10 @@ void idWindow::DrawBackground(const idRectangle &drawRect) {
 	if ( background && matColor.w() ) {
 		float scalex, scaley;
 		if ( flags & WIN_NATURALMAT ) {
-			scalex = drawRect.w / background->GetImageWidth();
-			scaley = drawRect.h / background->GetImageHeight();
+			// DG: now also multiplied with matScalex/y, don't see a reason not to support that
+			//     (it allows scaling a tiled background image)
+			scalex = (drawRect.w / background->GetImageWidth()) * matScalex;
+			scaley = (drawRect.h / background->GetImageHeight()) * matScaley;
 		} else {
 			scalex = matScalex;
 			scaley = matScaley;
