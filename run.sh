f1bf617d8f1d1fab89e67efd0d69bf756a34a5b9
MidnightHammer <midnighthammer66@gmail.com>
2026-03-29 09:35:09 -0400

    Fix color variable names in run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # This source code is licensed under the GPL 3.0 license
 # LICENSE file in the root directory of this source tree.
 
-# Color Definitions
+# Color definitions
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color (Reset)
+NONE='\033[0m'
 
 ALL_PRESENT=true
 
@@ -14,9 +14,9 @@ for i in {0..8}; do
     FILE="pak00${i}.pk4"
     
     if [ -f "base/${FILE}" ]; then
-        echo -e "[ ${GREEN}OK${NC} ] ${FILE}"
+        echo -e "[ ${GREEN}OK${NONE} ] ${FILE}"
     else
-        echo -e "[ ${RED}FAIL${NC} ] Missing: ${FILE}"
+        echo -e "[ ${RED}FAIL${NONE} ] Missing: ${FILE}"
         ALL_PRESENT=false
     fi
 done
@@ -24,11 +24,11 @@ done
 echo "----------------------------------------"
 
 if [ "$ALL_PRESENT" = true ]; then
-    echo -e "${GREEN}Success! All required .pk4 files are present.${NC}"
+    echo -e "${GREEN}Success! All required .pk4 files are present.${NONE}"
     echo "Running game ..."
     echo "Happy Gaming"
     exit 0
 else
-    echo -e "${RED}Error: You are missing some .pk4 files. Please add all pk4 to the base/ folder.${NC}"
+    echo -e "${RED}Error: You are missing some .pk4 files. Please add all pk4 to the base/ folder.${NONE}"
     exit 1
 fi
